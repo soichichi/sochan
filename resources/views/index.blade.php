03ee213105fe465416c4b3261ccdb1836734e410
soichichi <souichi.t425@gmail.com>
2021-11-20 18:01:45 +0900

    記事一覧画面にページネーション追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,5 +20,9 @@
                 </div>
             @endforeach
         </div>
+        
+        <div class = 'paginate'>
+            {{ $posts->links() }}
+        </div>
     </body>
 </html>
