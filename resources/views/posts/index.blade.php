6a9cd9b0acb5ed6376b15aa6d3b50d0c62cb2bcb
soichichi <souichi.t425@gmail.com>
2021-11-25 23:27:46 +0900

    ブログ投稿画 面の作成と保存処理実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,6 +12,7 @@
     
     <body>
         <h1>Blog Name</h1>
+        <p class = 'create'>[<a href='/posts/create'>create</a>]</p>
         <div class = 'posts'>
             @foreach ($posts as $post)
                 <div class ='post'>
