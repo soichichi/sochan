e9bb8d3c57dad95268a320c99d40cd61307c3e95
soichichi <souichi.t425@gmail.com>
2021-11-27 17:30:32 +0900

    ブログ投稿の編集

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,5 +14,7 @@
 
 Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::put('/posts/{post}', 'PostController@update');
 Route::get('/posts/{post}', 'PostController@show');
 Route::post('/posts', 'PostController@store');
