f1fc0a0105e20dda4a3886ed9f191b8e5be9f017
soichichi <souichi.t425@gmail.com>
2021-11-30 18:02:05 +0900

    ブログ投稿の消去

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,17 +7,30 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-    
     </head>
     
     <body>
         <h1>Blog Name</h1>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action = "/posts/{{$post->id}}" id = "form_delete" method ="post">
+            {{ csrf_field() }}
+            {{ method_field('delete') }}
+            <input type = 'submit' style = "display:none">
+            <p class = 'delete'>[<span onclick = 'return deletePost(this);'>delete</span>]</p>
+        </form>
         <div class ='post'>
             <h2 class ='title'>{{ $post->title }}</h2>
             <p class ='body'>{{ $post->body }}</p>
             <p class ='updated_at'>{{ $post->updated_at }}</p>
         </div>
         <div class = 'back'>[<a href = '/'>back</a>]</div>
+        <script>
+        function deletePost(e){
+            'use strict';
+            if (confirm('消去すると復元できません。\n本当に削除しますか?')){
+                document.getElementById('form_delete').submit();
+            }
+        }
+        </script>
     </body>
 </html>
